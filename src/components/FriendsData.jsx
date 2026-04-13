8e477804170044a36ff880ae097ecc53aa5a7f0d
SUMON <sumon01333581557.gmail.com>
2026-04-13 23:48:12 +0600

    Bug fixed

--- src/components/FriendsData.jsx
+++ src/components/FriendsData.jsx
@@ -1,30 +1,19 @@
 import FriendCard from "@/Ui/FriendCard";
+import friendsData from '@/../public/data.json';
 
 const FriendsData = async () => {
-    const res = await fetch(
-        `${process.env.NEXT_PUBLIC_BASE_URL || ""}/data.json`,
-        {
-            cache: "no-store",
-        }
-    );
-
-    const friendsData = await res.json();
 
     return (
-        <div className="bg-base-200">
-            <div className="max-w-7xl mx-auto py-7 md:py-12 border-b">
-                <h3 className="font-semibold text-2xl sm:text-3xl pb-5">
-                    Your Friends
-                </h3>
-
-                <div className="grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 md:gap-8">
-                    {friendsData.map((friend) => (
-                        <FriendCard key={friend.id} friend={friend} />
-                    ))}
+        <div className="bg-base-200  ">
+            <div className=" max-w-7xl mx-auto py-7 md:py-12 border-b">
+                <h3 className=" font-semibold text-2xl sm:text-3xl pb-5"> Your Friends</h3>
+                <div className=" grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 md:gap-8">
+                    {
+                        friendsData.map(friend => <FriendCard key={friend.id} friend={friend} />)
+                    }
                 </div>
             </div>
         </div>
     );
 };
-
 export default FriendsData;
