6c79b84f14d5d95050ae4a2d658a09c2b56dcabd
SUMON <sumon01333581557.gmail.com>
2026-04-13 22:40:29 +0600

    Problem solved

--- src/components/FriendsData.jsx
+++ src/components/FriendsData.jsx
@@ -1,21 +1,28 @@
 import FriendCard from "@/Ui/FriendCard";
+import fs from "fs/promises";
+import path from "path";
 
 const FriendsData = async () => {
-    const res = await fetch("http://localhost:3000/data.json");
-    const friendsData = await res.json()
+    const filePath = path.join(process.cwd(), "public/data.json");
 
+    const jsonData = await fs.readFile(filePath, "utf-8");
+    const friendsData = JSON.parse(jsonData);
 
     return (
-        <div className="bg-base-200  ">
-            <div className=" max-w-7xl mx-auto py-7 md:py-12 border-b">
-                <h3 className=" font-semibold text-2xl sm:text-3xl pb-5"> Your Friends</h3>
-                <div className=" grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 md:gap-8">
-                    {
-                        friendsData.map(friend => <FriendCard key={friend.id} friend={friend} />)
-                    }
+        <div className="bg-base-200">
+            <div className="max-w-7xl mx-auto py-7 md:py-12 border-b">
+                <h3 className="font-semibold text-2xl sm:text-3xl pb-5">
+                    Your Friends
+                </h3>
+
+                <div className="grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 md:gap-8">
+                    {friendsData.map((friend) => (
+                        <FriendCard key={friend.id} friend={friend} />
+                    ))}
                 </div>
             </div>
         </div>
     );
 };
+
 export default FriendsData;
